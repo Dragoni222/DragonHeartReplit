c9e4a4ee792d88ee05e8b158dd8b8212cb783f03
Joel Harrison <10212005@students.usd497.org>
2020-10-21 14:05:47 +0000

    Push so I can use it in visual studio.

--- ReadMapInput.cs
+++ ReadMapInput.cs
@@ -19,18 +19,14 @@ public static List<List<string>> readFullMap(List<List<string>> original, string
 
 {
     int arrayX = 0;
-
+    int arrayY = 0;
     bool inQuote = false;
 
     for (int i = 0; i <= newString.Length - 1; i++)
     {
-        if (newString[i].ToString() == "{")
-        {
-            if (i != 0)
-                arrayX++;
-        }
+       
 
-        else if (newString[i].ToString() == "\"")
+        if (newString[i].ToString() == "\"")
         {
             if (inQuote == false)
             {
@@ -44,9 +40,18 @@ public static List<List<string>> readFullMap(List<List<string>> original, string
 
         }
 
-        else if (inQuote == true && newString[i].ToString() != "," && newString[i].ToString() == " ")
+        else if (newString[i].ToString() == "}")
         {
-            original[arrayX][i] = newString[i].ToString();
+          arrayX++;
+          arrayY = 0;
+        }
+         else if (newString[i].ToString() == ",")
+          arrayY++;
+
+        else if (inQuote == true && newString[i].ToString() == " "&& newString[i].ToString() != "{"&& newString[i].ToString() != ";")
+        {
+            original[arrayX][arrayY] = newString[i].ToString();
+            
         }
     }
 
@@ -56,20 +61,16 @@ public static List<List<string>> readFullMap(List<List<string>> original, string
 public static List<List<ConsoleColor>> readFullMapColor(List<List<ConsoleColor>> original, string newString)
 {
     int arrayX = 0;
+    int arrayY = -1;
     bool inQuote = false;
     string addedTogether = "";
-    string addedTogetherConvertable = "                            ";
-    int pos1;
-    int pos2;
+    StringBuilder sb = new StringBuilder(addedTogether, 50);
+
 
     for (int i = 0; i <= newString.Length - 1; i++)
     {
-        if (newString[i].ToString() == "{")
-        {
-            if (i != 0)
-                arrayX++;
-        }
-        else if (newString[i].ToString() == "\"")
+        
+        if (newString[i].ToString() == "\"")
         {
              if (inQuote == false)
             {
@@ -78,24 +79,30 @@ public static List<List<ConsoleColor>> readFullMapColor(List<List<ConsoleColor>>
 
             if (inQuote == true)
             {
-              
-              for(int j = 1; j <= pos2-pos1; j++)
-              {
-                addedTogetherConvertable[i] = addedTogether[j+13];
-              }
 
-              addedTogetherConvertable = addedTogether[13].ToLower() + addedTogetherConvertable;
+                addedTogether = sb.ToString();
                 inQuote = false;
-                original[arrayX, i] = convertColorToConsoleColor(addedTogetherConvertable);
+                original[arrayX][arrayY] = convertColorToConsoleColor(addedTogether);
+                addedTogether = "";
+                sb = new StringBuilder();
+                
             }
         }
         
-
-
-        else if (newString[i].ToString() != "," && newString[i].ToString() == " ")
+        else if (newString[i].ToString() == "}")
         {
-            if (inQuote == true)
-                addedTogether += newString[i];
+          arrayX++;
+          arrayY = 0;
+        }
+        else if (newString[i].ToString() == ",")
+          arrayY++;
+
+        else if (inQuote == true &&  newString[i].ToString() == " "&& newString[i].ToString() != "{"&& newString[i].ToString() != ";")
+        {
+          
+          sb.Append(newString[i].ToString());
+              
+            
         }
     }
 
